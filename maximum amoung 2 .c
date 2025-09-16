7b670721ebf899dfb588d29a71a8f54e0b98ec4f
PrNV <pranavsagar801@gmail.com>
2025-09-17 00:11:18 +0530

    Add files via upload

---
+++ maximum amoung 2 .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(){
+int a,b,max;
+
+printf("enter the value of a:");
+scanf("%d",&a);
+printf("enter the value of b:");
+scanf("%d",&b);
+
+
+max = (a>b)?a:b;
+
+printf("max=%d",max);
+
+}
