d354170a4ed52816ffdb068711942a7f6ee18be8
PrNV <pranavsagar801@gmail.com>
2025-09-17 13:15:58 +0530

    Add files via upload

---
+++ printing name n times.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int i,n;
+    printf("Enter how many times you want your name:");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++){
+    printf("me PrNV!\n");
+    }
+
+}
